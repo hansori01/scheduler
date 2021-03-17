545a4189a2d2ed9a5a42ebb71e704c2bd7fa5228
hansori01 <19sorihan88@gmail.com>
2021-03-17 16:41:59 -0700

    using axios to fetch days

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
 
 import DayList from "./DayList";
 import Appointment from "components/Appointment";
@@ -47,33 +48,23 @@ const appointmentsArray = [
   },
 ];
 
-
-
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
-
 const appointments = appointmentsArray.map(appointment => {
   return <Appointment key={appointment.id} {...appointment} />
 })
 
 export default function Application() {
 
+  const [days, setDays] = useState([]);
   const [day, setDay] = useState('Monday');
+
+  useEffect(() => {
+    //proxy added to package.json to avoid CORS error
+    axios.get('http://localhost:8001/api/days').then(response => {
+      // console.log(response.data)
+      setDays([...response.data])
+    });
+  }, []); //empty dependency as we only want this run at the top of the render
+
 
   return (
     <main className="layout">
@@ -106,4 +97,4 @@ export default function Application() {
       </section>
     </main>
   );
-}
+};

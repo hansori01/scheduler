ec3aab9e1b211911bf500b3b4f999159f4c5bfbb
hansori01 <19sorihan88@gmail.com>
2021-03-16 17:25:42 -0700

    adding confirm component

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 
-import Show from './Show'
-
 import './style.scss';
 
 
--- stories/index.js
+++ stories/index.js
@@ -14,6 +14,7 @@ import Appointment from "components/Appointment/index";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
+import Confirm from "components/Appointment/Confirm";
 
 import "index.scss";
 
@@ -161,6 +162,13 @@ storiesOf("Appointment", module)
       student={'Lydia Miller-Jones'}
       onEdit={action('onEdit')}
       onDelete={action('onDelete')}
+    />
+  ))
+  .add("Confirm", () => (
+    <Confirm
+      message='Delete the appointment?'
+      onConfirm={action('onConfirm')}
+      onCancel={action('onCancel')}
     />
   ))
 

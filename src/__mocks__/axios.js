10f0b3053d5ec7b789e61f79a9a08661a6dcfc7e
hansori01 <19sorihan88@gmail.com>
2021-03-23 12:10:48 -0700

    adding local mock axios.get function referring to fixture data

--- src/__mocks__/axios.js
+++ src/__mocks__/axios.js
@@ -54,3 +54,29 @@ const fixtures = {
     }
   }
 };
+
+export default {
+  get: jest.fn(url => {
+    if (url === "/api/days") {
+      return Promise.resolved({
+        status: 200,
+        statusText: "OK",
+        data: fixtures.days
+      });
+    }
+    if (url === "/api/appointments") {
+      return Promise.resolved({
+        status: 200,
+        statusText: "OK",
+        data: fixtures.appointments
+      });
+    }
+    if (url === "/api/interviewers") {
+      return Promise.resolved({
+        status: 200,
+        statusText: "OK",
+        data: fixtures.interviewers
+      });
+    }
+  })
+};

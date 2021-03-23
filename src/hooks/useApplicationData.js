cfde504e33c2f594c8692c9f6ebfd4a01f37876c
hansori01 <19sorihan88@gmail.com>
2021-03-23 12:10:23 -0700

    refactoring links

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -34,7 +34,7 @@ export default function useApplicationData() {
     const appointments = { ...state.appointments, [id]: appointment };
     const days = updateSpot(-1)
 
-    return axios.put(`http://localhost:8001/api/appointments/${id}`, { interview })
+    return axios.put(`api/appointments/${id}`, { interview })
       .then(res => { setState({ ...state, appointments, days }) });
   };
 
@@ -44,7 +44,7 @@ export default function useApplicationData() {
     const appointments = { ...state.appointments, [id]: appointment };
     const days = updateSpot(+1)
 
-    return axios.delete(`http://localhost:8001/api/appointments/${id}`, appointment)
+    return axios.delete(`/api/appointments/${id}`, appointment)
       .then(res => setState({ ...state, appointments, days }));
   };
 
@@ -56,9 +56,9 @@ export default function useApplicationData() {
   //fetch and update appointment / days data 
   useEffect(() => {
     Promise.all([
-      axios.get('http://localhost:8001/api/days'),
-      axios.get('http://localhost:8001/api/appointments'),
-      axios.get('http://localhost:8001/api/interviewers')
+      axios.get('/api/days'),
+      axios.get('/api/appointments'),
+      axios.get('/api/interviewers')
     ]).then(all => {
       const days = all[0].data;
       const appointments = all[1].data;

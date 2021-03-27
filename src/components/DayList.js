fd299d92eb8f895a8c0e37ed030f4d339ddbab2a
hansori01 <19sorihan88@gmail.com>
2021-03-27 13:50:24 -0700

    removing function back to DayListItem to keep it local, yet out of multiple run

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -4,27 +4,15 @@ import DayListItem from './DayListItem';
 
 export default function DayList(props) {
 
-  const days = props.days.map(day => {
-    function formatSpots() {
-      if (day.spots > 1) {
-        return day.spots + ' spots remaining';
-      } else if (day.spots === 1) {
-        return day.spots + ' spot remaining';
-      }
-      return 'no spots remaining';
-    };
-
-    return (
-      <DayListItem
-        key={day.id}
-        name={day.name}
-        spots={day.spots}
-        formatSpots={formatSpots()}
-        selected={day.name === props.day}
-        setDay={() => props.setDay(day.name)}
-      />
-    )
-  });
+  const days = props.days.map(day =>
+    <DayListItem
+      key={day.id}
+      name={day.name}
+      spots={day.spots}
+      selected={day.name === props.day}
+      setDay={() => props.setDay(day.name)}
+    />
+  );
 
   return (
     <ul>

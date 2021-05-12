881bd2f0099cf703147b93e1e6cd1ee39d8e5b6a
hansori01 <19sorihan88@gmail.com>
2021-05-12 16:55:08 -0700

    checking to see if hoisting issue

--- src/index.js
+++ src/index.js
@@ -3,12 +3,11 @@ import ReactDOM from "react-dom";
 
 import "index.scss";
 import axios from "axios";
+import Application from "components/Application";
 
 // if (process.env.REACT_APP_API_BASE_URL) {
 // axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
 axios.defaults.baseURL = "https://schedulerreact.herokuapp.com/";
 // }
-
-import Application from "components/Application";
 
 ReactDOM.render(<Application />, document.getElementById("root"));
